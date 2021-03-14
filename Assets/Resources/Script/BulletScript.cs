cfd2e658fbe19571af52737cfacc81ec97e76755
Leo Harnadi <leo.h.m@mail.ugm.ac.id>
2021-03-14 16:19:07 +0700

    Week 4

---
+++ Assets/Resources/Script/BulletScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playAudio("gunfire");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            playAudio("hit");
+            Destroy(gameObject.GetComponent<SpriteRenderer>());
+            Destroy(gameObject.GetComponent<Collider2D>());
+            StartCoroutine(DestroyAfterTime(2, gameObject));
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Wall"))
+        {
+            playAudio("thud");
+            source.time = 0.2f;
+            Destroy(gameObject.GetComponent<SpriteRenderer>());
+            Destroy(gameObject.GetComponent<Collider2D>());
+            StartCoroutine(DestroyAfterTime(2, gameObject));
+        }
+    }
+
+    public void playAudio (string audioName)
+    {
+        source = gameObject.AddComponent<AudioSource>();
+        source.clip = (AudioClip)Resources.Load("Audio/" + audioName);
+        source.volume = 0.15f;
+        source.Play();
+    }
+
+    IEnumerator DestroyAfterTime(float time, GameObject dead)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(dead.gameObject);
+    }
+}
---
+++ Assets/Resources/Script/PauseScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public bool gameOver = false;
+
+    public Text pauseText;
+
+    public GameObject pauseScreen;
+
+    AudioSource pauseAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+
+        pauseText = GameObject.Find("Pause Text").GetComponent<Text>();
+
+        pauseScreen.SetActive(false);
+
+        pauseAudio = gameObject.AddComponent<AudioSource>();
+        pauseAudio.clip = (AudioClip)Resources.Load("Audio/pause");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && gameOver == false)
+        {
+            PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        gamePaused = !gamePaused;
+
+        if(gamePaused)
+        {
+            pauseAudio.Play();
+
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        } else
+        {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GoHome()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
